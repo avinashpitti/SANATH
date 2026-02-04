aed2f7a624ff1b66cc1c1d14715d20c2b49f7e21
avinashpitti <avinash87129@gmail.com>
2026-02-04 18:56:22 +0530

    if_elif_else condition

---
+++ if_else.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+
+if a ==b :
+    print(f'Both {a} and {b} are equal')
+elif a < b :
+    print(f'{a} is less than {b}')
+else :
+    print(f'{a} is greater than {b}')
